5e67af6879343ce8aec90b7c895bcafc04914303
HelloVine <viniciusjordao.contact@gmail.com>
2020-04-10 21:36:13 -0300

    I doed a function for to create the sections before's show in display and hover another element is ready

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,26 @@ $(document).ready(function () {
    
     var sections = ['hover-another-element', 'input-placeholder-to-label'];
 
+    function createSection(section, index) {
+
+        var article = document.getElementsByTagName("article");
+
+        var getSection = document.getElementsByTagName("section");
+
+        var createSection = document.createElement("section");
+
+        article[0].after(createSection);
+
+        setTimeout(function () {
+
+            getSection[index].setAttribute('class', section);
+            
+        }, 1);
+
+    }
+
+    sections.forEach(createSection);
+
     function showSection(section) {
 
         var classSection = '.' + section;
@@ -15,7 +35,11 @@ $(document).ready(function () {
         });
         
     }
+    
+    setTimeout(function () {
+       
+        sections.forEach(showSection);
 
-    sections.forEach(showSection);
+    }, 2);
 
 });
